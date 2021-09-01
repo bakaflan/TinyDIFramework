626863ee229daa0b0b36d46491c43ac06de986ee
Ruiling Guo <ruiling.guo@thoughtworks.com>
2021-09-01 11:01:16 +0800

    FEAT:  add exception test for circle dependence

--- src/test/java/livedoc/fixtures/ContainerTest.java
+++ src/test/java/livedoc/fixtures/ContainerTest.java
@@ -3,6 +3,7 @@ package livedoc.fixtures;
 import com.bakaflan.di.ClassUtils;
 import com.bakaflan.di.Container;
 import com.bakaflan.di.example.Foo;
+import com.bakaflan.di.exception.CreatInstanceErrorException;
 import com.thoughtworks.fusheng.integration.junit5.FuShengTest;
 
 @FuShengTest
@@ -16,6 +17,15 @@ public class ContainerTest {
 
     public Object getInstance(String className) throws ClassNotFoundException {
         return container.getInstance(ClassUtils.getClassByName(className));
+    }
+
+    public String getCircleDependInstance(String className){
+            try{
+                container.getInstance(ClassUtils.getClassByName(className));
+                return "创建实例成功";
+            }catch (CreatInstanceErrorException | ClassNotFoundException e){
+                return "创建实例失败";
+            }
     }
 
     public String getClassName(Object clazz) {
